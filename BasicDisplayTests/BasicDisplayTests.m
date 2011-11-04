b84e0664976f46c6a2b94e168156be06aa8ea3f8
Grayson Smith <d.grayson.smith@gmail.com>
2011-11-04 14:00:18 -0700

    Initial Commit

---
+++ BasicDisplayTests/BasicDisplayTests.m
@@ -0,0 +1,32 @@
+//
+//  BasicDisplayTests.m
+//  BasicDisplayTests
+//
+//  Created by Grayson Smith on 11/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BasicDisplayTests.h"
+
+@implementation BasicDisplayTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BasicDisplayTests");
+}
+
+@end

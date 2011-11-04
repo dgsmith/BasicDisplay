d128030599f8310074096c954241889d272313da
Grayson Smith <d.grayson.smith@gmail.com>
2011-11-04 15:38:16 -0700

    initial commit

--- BasicDisplay/ViewController.h
+++ BasicDisplay/ViewController.h
@@ -8,6 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate>
+{
+
+    UIView *containerView;
+    UIToolbar *toolbar;
+    UIImageView *imageView;
+    
+    NSMutableArray *images;
+    NSURL *url;
+    
+}
+
+@property (nonatomic, retain) UIPopoverController *popoverController;
+@property (nonatomic, retain) IBOutlet UIView *containerView;
+@property (nonatomic, retain) IBOutlet UIView *toolbar;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *photosButton;
+
+-(IBAction)choosePhotos:(id)sender;
 
 @end
--- BasicDisplay/ViewController.m
+++ BasicDisplay/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize containerView;
+@synthesize toolbar;
+@synthesize photosButton;
+@synthesize popoverController;
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,10 +27,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    images = [[NSMutableArray alloc] init];
+
 }
 
 - (void)viewDidUnload
 {
+    containerView = nil;
+    toolbar = nil;
+    [self setContainerView:nil];
+    [self setToolbar:nil];
+    [self setPhotosButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -55,6 +68,31 @@
 {
     // Return YES for supported orientations
     return YES;
+}
+
+-(IBAction)choosePhotos:(id)sender
+{
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    UIPopoverController *popover = [[UIPopoverController alloc] initWithContentViewController:imagePickerController];
+    popover.contentViewController = imagePickerController;
+    [popover presentPopoverFromBarButtonItem:photosButton permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    self.popoverController = popover;
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
+{       
+    imageView = [[UIImageView alloc] initWithImage:image];
+    
+    [containerView addSubview:imageView];
+    
+    [popoverController dismissPopoverAnimated:YES];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [popoverController dismissPopoverAnimated:YES];
 }
 
 @end
